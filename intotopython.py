6f083b75dfba9d37c11d1a28f9ad94f9b6737719
Davon Wilson <davontech3@gmail.com>
2023-12-07 23:10:27 -0500

    part 2 intro python

---
+++ intotopython.py
@@ -0,0 +1,12 @@
+fruit = 4
+print(fruit)
+print(type(fruit))
+e, f, g = 1, 2, 3
+print(e, f, g) 
+mylist = [1,2,3,4,5]
+mylist.append(9)
+mylist.insert(3,32)
+mylist.sort()
+mylist.reverse()
+
+print(mylist)
